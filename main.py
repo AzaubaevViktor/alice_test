074dc9a0cfebb0a021670c67a382011339653725
Виктор <sarvash.inc@gmail.com>
2018-07-27 12:28:50 +0300

    Add webhook

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ async def handle(request):
     text = "Hello, " + name
     return web.Response(text=text)
 
+
+async def webhook(request):
+    text = """<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>Verification: e61e2475dcfaa0e7</body>
+</html>"""
+
 app = web.Application()
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/{name}', handle),
+                web.get('/yandex_e61e2475dcfaa0e7.html', webhook)])
 
 web.run_app(app, host="0.0.0.0", port=5000)
