948b7000d4182448b306f494842c720ce0cdd9db
Виктор <sarvash.inc@gmail.com>
2018-07-27 12:33:47 +0300

    Add port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from aiohttp import web
-
+import os
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
@@ -21,4 +21,4 @@ app.add_routes([web.get('/', handle),
                 web.get('/{name}', handle),
                 web.get('/yandex_e61e2475dcfaa0e7.html', webhook)])
 
-web.run_app(app, host="0.0.0.0", port=5000)
+web.run_app(app, host="0.0.0.0", port=os.environ['PORT'] or 8080)
